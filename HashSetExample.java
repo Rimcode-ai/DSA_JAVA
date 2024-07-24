48ade3a6fed67323636e5d5e7e4ac751a7711a7c
Rima Modak <sas999modak111@gmail.com>
2024-07-24 00:04:06 +0000

    Hashset theory

--- HashSetExample.java
+++ HashSetExample.java
@@ -1,5 +1,26 @@
 import java.util.HashSet;
 
+/*
+Part of java's collection framework
+Stores unique elements in an unordered manner
+Derived from AbstractSet class & implements the set interface
+Used when dealing with a collection of unique items.
+Provides efficient membership checks & remove duplicates
+ */
+
+/*
+Complexity analysis of Hashset Operations:
+Time Complexity: O(1)
+Space Complexity: O(n),  Linear
+ */
+
+/*
+Usage: Large Datasets,
+Efficient for adding, checking, deleting items in a collection
+Hashsets are applicable in tackling unique visited web pages
+
+Efficiency: HashSets are like Mathematical sets- only unique type
+ */
 public class HashSetExample {
     public static void main(String[] args) {
         System.out.println("We are learning Java HashSet");
